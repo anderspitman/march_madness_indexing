a4ce2fc9dc59ca7aa7eba486e331901350570132
Anders Pitman <tapitman11@gmail.com>
2017-03-24 09:30:38 -0700

    Added some stats
    Leaderboard and overall record count

--- index.js
+++ index.js
@@ -1,49 +1,7 @@
 (function(d3, firebase) {
 
-  var firstRoundData = [
-    [
-      { "name": "university", "x": 54, "y": 101 },
-      { "name": "san_marcos", "x": 151, "y": 95 },
-      { "name": "horizon", "x": 213, "y": 121 }
-    ],
-    [
-      { "name": "south_mountain", "x": 291, "y": 31 },
-      { "name": "towne_lake", "x": 365, "y": 22 },
-      { "name": "pioneer", "x": 452, "y": 30 }
-    ],
-    [
-      { "name": "mission_bay", "x": 569, "y": 88 },
-      { "name": "mcclintock", "x": 650, "y": 62 },
-      { "name": "southshore", "x": 719, "y": 110 }
-    ]
-  ];
-
-  var secondRoundData = [
-    [
-      { "name": "???", "x": 103, "y": 265 },
-      { "name": "???", "x": 63, "y": 335 }
-    ],
-    [
-      { "name": "???", "x": 335, "y": 192 },
-      { "name": "???", "x": 430, "y": 188 }
-    ],
-    [
-      { "name": "???", "x": 664, "y": 268 },
-      { "name": "???", "x": 576, "y": 209 }
-    ]
-  ];
-
-  var thirdRoundData = [
-    [
-      { "name": "???", "x": 282, "y": 365 },
-      { "name": "???", "x": 364, "y": 323 },
-      { "name": "???", "x": 446, "y": 345 }
-    ]
-  ];
-
-  var allData;
-  var width;
   var wardInfo;
+  var allData;
   var treeXml;
 
   d3.xml("assets/tree.svg").mimeType("image/svg+xml").get(function(error, xml) {
@@ -55,8 +13,9 @@
   });
 
 
-
   function firebaseStuff() {
+
+    var contributorStats;
 
     var config = {
       apiKey: "AIzaSyAkHMLIBu1BMxSSrv7jX_6wnbg2WnujlCg",
@@ -74,51 +33,110 @@
 
       wardInfo = snapshot.val();
 
+      return db.ref().child('contributors').limitToLast(1).once('value');
+    })
+    .then(function(snapshot) {
+
+      contributorStats = getFirstItem(snapshot.val());
+
       return db.ref().child('stake_and_ward_indexing').limitToLast(1).once('value');
     })
     .then(function(snapshot) {
       var val = snapshot.val();
 
-      makeCharts(getFirstItem(val));
+      makeCharts(getFirstItem(val), contributorStats);
     });
   }
 
-  function makeCharts(data) {
+  function makeCharts(data, contributorStats) {
 
     console.log("Processing data from:", data.timestamp);
     allData = data.units;
 
-    var container = d3.select('.tree-container')
-        .node().appendChild(treeXml.documentElement);
-
+    d3.select('.tree-container')
+      .node().appendChild(treeXml.documentElement);
 
     var svg = d3.select("svg")
-        //.attr("width", "200px")
-        //.attr("height", "200px");
         .attr("width", "100%")
         .attr("height", "100%");
 
-    width = parseInt(svg.style("width"));
+    var tree = treeChart();
+    svg.call(tree);
 
-    var firstRound = firstRoundChart();
+    var stats = d3.select('.stats-container').append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%");
 
-    svg.append("g")
-        .datum(firstRoundData)
-        .attr("class", "first-round")
-        .call(firstRound.round("first"));
-
-    svg.append("g")
-        .datum(secondRoundData)
-        .attr("class", "second-round")
-        .call(firstRound.round("second"));
-
-    svg.append("g")
-        .datum(thirdRoundData)
-        .attr("class", "third-round")
-        .call(firstRound.round("third"));
+    stats.call(statsChart().data(contributorStats));
   }
 
-  function firstRoundChart() {
+  function treeChart() {
+
+    var firstRoundData = [
+      [
+        { "name": "university", "x": 54, "y": 101 },
+        { "name": "san_marcos", "x": 151, "y": 95 },
+        { "name": "horizon", "x": 213, "y": 121 }
+      ],
+      [
+        { "name": "south_mountain", "x": 291, "y": 31 },
+        { "name": "towne_lake", "x": 365, "y": 22 },
+        { "name": "pioneer", "x": 452, "y": 30 }
+      ],
+      [
+        { "name": "mission_bay", "x": 569, "y": 88 },
+        { "name": "mcclintock", "x": 650, "y": 62 },
+        { "name": "southshore", "x": 719, "y": 110 }
+      ]
+    ];
+
+    var secondRoundData = [
+      [
+        { "name": "???", "x": 103, "y": 265 },
+        { "name": "???", "x": 63, "y": 335 }
+      ],
+      [
+        { "name": "???", "x": 335, "y": 192 },
+        { "name": "???", "x": 430, "y": 188 }
+      ],
+      [
+        { "name": "???", "x": 664, "y": 268 },
+        { "name": "???", "x": 576, "y": 209 }
+      ]
+    ];
+
+    var thirdRoundData = [
+      [
+        { "name": "???", "x": 282, "y": 365 },
+        { "name": "???", "x": 364, "y": 323 },
+        { "name": "???", "x": 446, "y": 345 }
+      ]
+    ];
+
+    var round = roundChart();
+
+    function my(selection) {
+
+      selection.append("g")
+          .datum(firstRoundData)
+          .attr("class", "first-round")
+          .call(round.round("first"));
+
+      selection.append("g")
+          .datum(secondRoundData)
+          .attr("class", "second-round")
+          .call(round.round("second"));
+
+      selection.append("g")
+          .datum(thirdRoundData)
+          .attr("class", "third-round")
+          .call(round.round("third"));
+      }
+
+    return my;
+  }
+
+  function roundChart() {
 
     var faceoff = faceoffChart();
     var round;
@@ -256,12 +274,133 @@
     return my;
   }
 
+  function statsChart() {
+
+    var shortNameMap = {
+      'south_mountain': 'SOMO',
+      'university': 'UNIV',
+      'san_marcos': 'SANM',
+      'towne_lake': 'TNLK',
+      'southshore': 'SOSH',
+      'horizon': 'HRZN',
+      'mcclintock': 'MCTK',
+      'mission_bay': 'MISB',
+      'pioneer': 'PION'
+    };
+
+    var data;
+
+    function my(selection) {
+      var totalRec = selection.append("g")
+          .attr("class", "total-records");
+
+      // Calculate total indexed records
+      var total = 0;
+      Object.keys(data.wards).forEach(function(ward_name) {
+        var ward = data.wards[ward_name];
+        ward.forEach(function(person) {
+          total += person.indexed;
+        });
+      });
+
+      totalRec.append("text")
+          .attr("font-size", 16)
+          .attr("font-weight", "bold")
+          .attr("x", 0)
+          .attr("y", 15)
+          .text("Total Records:");
+
+      totalRec.append("text")
+          .attr("x", 120)
+          .attr("y", 15)
+          .text(total);
+
+      var leaders = computeLeaderboard(data);
+
+      console.log(leaders);
+
+      var leaderboard = selection.append("g")
+          .attr("class", "leaderboard")
+          .attr("transform", svgTranslateString(0, 70));
+
+      leaderboard.append("text")
+          .attr("font-size", 16)
+          .attr("font-weight", "bold")
+          //.attr("text-anchor", "middle")
+          .attr("text-decoration", "underline")
+          //.attr("x", 70)
+          .text("Leaderboard (#Records)");
+
+      leaderboard.append("g")
+          .attr("class", "leaders")
+          .attr("transform", svgTranslateString(0, 20))
+        .selectAll(".leader")
+          .data(leaders)
+        .enter().append("g")
+          .attr("class", "leader")
+          .attr("transform", function(d, i) {
+            return svgTranslateString(0, i*20);
+          })
+        .append("text")
+          .text(function(d, i) {
+            var nameSplit = d.name.split(" ");
+
+            var name = nameSplit[0];
+
+            if (nameSplit.length === 1) {
+              name = nameSplit[0];
+            }
+            else {
+              name = nameSplit[0] + " " + nameSplit[nameSplit.length - 1];
+            }
+
+            return (i + 1) + " " + name + " (" + shortNameMap[d.ward_name] + ")" + " - " + d.indexed;
+          })
+    }
+
+    my.data = function(value) {
+      if (!arguments.length) return data;
+      data = value;
+      return my;
+    }
+
+    return my;
+  }
+
   function svgTranslateString(x, y) {
     return "translate(" + x + "," + y + ")";
   }
 
   function getFirstItem(obj) {
     return obj[Object.keys(obj)];
+  }
+
+  function computeLeaderboard(data) {
+
+    var dataArray = [];
+    Object.keys(data.wards).forEach(function(wardName) {
+      data.wards[wardName].forEach(function(person) {
+        person.ward_name = wardName;
+      });
+      dataArray.push(data.wards[wardName]);
+    });
+
+    var flat = dataArray.reduce(function(acc, val) {
+      return acc.concat(val);
+    });
+
+    var sorted = flat.sort(function(a, b) {
+      var keyA = a.indexed;
+      var keyB = b.indexed;
+
+      if (keyA < keyB) return -1;
+      if (keyA > keyB) return 1;
+      return 0;
+    }).reverse();
+
+    var leaders = sorted.slice(0, 20);
+
+    return leaders;
   }
 
 }(d3, firebase));
